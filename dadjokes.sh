475b0ea59a070b4628ec7605b41311af5b7d9130
sinabehmanesh <sina.behmanesh1999@gmail.com>
2024-08-11 14:25:53 +0200

    feat: macos support added

--- dadjokes.sh
+++ dadjokes.sh
@@ -5,17 +5,41 @@ RED='\033[0;31m'
 NC='\033[0m'
 Blue='\033[1;36m'
 
-#check if notify-send exists on this machine!
+#Get os name
+osType=$(uname -o)
 
-if ! command -v notify-send  $> /dev/null
-then
-	echo -e "${RED}ERROR: notify-send package not present${NC}"
-	echo -e "Install it using:
-	${Blue} apt-get install notify-send${NC}"
-	exit 1
+function dadjoke_linux() {
+	echo "Linux dadjoke!"
+}
+function dadjoke_macos() {
+	title="Dad Joke"	
+	joke=$(curl -s https://icanhazdadjoke.com)
+	iconfile="dad.png"
+	iconpath="/Users/sina/Documents/linux-tools/dad.png"
+	osascript <<EOF
+display notification "$joke" with title "$title" sound name "default"
+EOF
+}
+
+if [ "$osType" = "GNU/Linux" ]; then
+	if command -v notify-send > /dev/null; then
+		dadjoke_linux
+	else
+		echo -e "${RED}ERROR: notify-send package not present${NC}"
+		echo -e "Install it using:
+		${BLUE}apt-get install notify-send${NC}"
+		exit 1
+	fi
+elif [ "$osType" = "Darwin" ]; then
+	if command -v osascript > /dev/null; then
+		dadjoke_macos
+	else
+		echo -e "${RED}ERROR: osascript not found!${NC}"
+		exit 1
+	fi
+else
+	echo "OS not supported!"
 fi
 
-joke=$(curl -s https://icanhazdadjoke.com)
-iconfile="dad.png"
-iconpath="${0/dadjokes.sh/"$iconfile"}"
-notify-send -a dadjokes -u critical -i "${iconpath}"  "Joke" "${joke}"
+
+#notify-send -a dadjokes -u critical -i "${iconpath}"  "Joke" "${joke}"
